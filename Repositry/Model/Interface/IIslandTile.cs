ff2fb51fc6e21af4f6dd8e6a3b46fc6bc3535afd
Akilgour <kilgourkilgour@hotmail.com>
2014-08-30 22:44:27 +0100

    Added IIslandTile asnd some properties

---
+++ Repositry/Model/Interface/IIslandTile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositry.Model.Interface
+{
+    interface IIslandTile
+    {
+        int id { get; set; }
+        string name { get; set; }
+        bool isFlooded { get; set; }
+        Repositry.Repository.Enums.PlayerColours startingTileForPlayer { get; set; }
+        Repositry.Repository.Enums.Treasures canHaveTreasures { get; set; }
+        bool hasTreasures { get; set; }
+    }
+}
