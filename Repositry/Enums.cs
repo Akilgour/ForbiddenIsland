81d8ebe6ebf71572003d36f07422c3f92039011f
Akilgour <kilgourkilgour@hotmail.com>
2014-08-30 21:50:42 +0100

    Added file for holding enums, added the player colours to it

---
+++ Repositry/Enums.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositry
+{
+   public  class Enums
+    {
+       public enum PlayerColours
+       {
+           Red,
+           Green,
+           Yellow,
+           Purple,
+           Blue,
+           Silver
+       }
+    }
+}
