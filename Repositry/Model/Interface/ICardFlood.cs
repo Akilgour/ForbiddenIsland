930f3fff5c9c293c1f8195aff82abf1758636589
Akilgour <kilgourkilgour@hotmail.com>
2014-08-31 23:23:23 +0100

    Added the flood card

---
+++ Repositry/Model/Interface/ICardFlood.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositry.Model.Interface
+{
+    public interface ICardFlood
+    {
+        int islandTileid { get; set; }
+        string islandTile { get; set; }
+    }
+}
