001ffd588277225c92ddfbf77563496b808e928d
Akilgour <kilgourkilgour@hotmail.com>
2014-09-01 00:22:56 +0100

    Added a model for holding the island

---
+++ Repositry/Model/Interface/IIsland.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositry.Model.Interface
+{
+    public interface IIsland
+    {
+        DataTable islandBoard { get; set; }
+    }
+}
